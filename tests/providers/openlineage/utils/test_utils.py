d5ea547635e09998a077e172c798199b912df58b
Anandhi <anandhi_murali@apple.com>
2024-04-12 16:28:15 -0700

    Ability to add custom facet in OpenLineage events

--- tests/providers/openlineage/utils/test_utils.py
+++ tests/providers/openlineage/utils/test_utils.py
@@ -18,15 +18,33 @@
 from __future__ import annotations
 
 import pytest
+from unittest import mock
 
+from airflow import DAG
 from airflow.decorators import task_group
 from airflow.models.taskinstance import TaskInstance as TI
 from airflow.operators.empty import EmptyOperator
+from airflow.providers.openlineage.conf import custom_facet_functions
 from airflow.providers.openlineage.plugins.facets import AirflowMappedTaskRunFacet
 from airflow.providers.openlineage.utils.utils import get_custom_facets
 from airflow.utils import timezone
+from airflow.utils.state import State
+from tests.test_utils.config import conf_vars
 
 DEFAULT_DATE = timezone.datetime(2016, 1, 1)
+SAMPLE_TI = TI(
+    task=EmptyOperator(task_id="test-task", dag=DAG("test-dag"), start_date=DEFAULT_DATE),
+    state=State.RUNNING.value,
+)
+
+
+@pytest.fixture(autouse=True)
+def clear_cache():
+    custom_facet_functions.cache_clear()
+    try:
+        yield
+    finally:
+        custom_facet_functions.cache_clear()
 
 
 @pytest.mark.db_test
@@ -48,3 +66,83 @@ def test_get_custom_facets(dag_maker):
             mapIndex=0,
             operatorClass=f"{ti_0.task.operator_class.__module__}.{ti_0.task.operator_class.__name__}",
         )
+
+
+@mock.patch.dict("os.environ", {})
+def test_get_custom_facets_with_no_function_definition():
+    result = get_custom_facets(SAMPLE_TI)
+    assert result == {}
+
+
+@conf_vars(
+    {
+        (
+            "openlineage",
+            "custom_facet_functions",
+        ): "tests.providers.openlineage.utils.custom_facet_fixture.get_additional_test_facet"
+    }
+)
+def test_get_custom_facets_with_function_definition():
+    result = get_custom_facets(SAMPLE_TI)
+    assert result == {
+        "additional_run_facet": {
+            "_producer": "https://github.com/apache/airflow/tree/providers-openlineage/1.7.0",
+            "_schemaURL": "https://raw.githubusercontent.com/OpenLineage/OpenLineage/main/spec/OpenLineage.json#/definitions/BaseFacet",
+            "name": "test-lineage-namespace",
+            "jobState": "running",
+            "uniqueName": "TEST.test-dag.test-task",
+            "displayName": "test-dag.test-task",
+            "dagId": "test-dag",
+            "taskId": "test-task",
+            "cluster": "TEST",
+        }
+    }
+
+
+@conf_vars(
+    {
+        (
+            "openlineage",
+            "custom_facet_functions",
+        ): "tests.providers.openlineage.utils.custom_facet_fixture.get_additional_test_facet; ;"
+        "invalid_function; tests.providers.openlineage.utils.custom_facet_fixture.return_type_is_not_dict;"
+        " tests.providers.openlineage.utils.custom_facet_fixture.get_another_test_facet "
+    },
+)
+def test_get_custom_facets_with_multiple_function_definition():
+    result = get_custom_facets(SAMPLE_TI)
+    assert result == {
+        "additional_run_facet": {
+            "_producer": "https://github.com/apache/airflow/tree/providers-openlineage/1.7.0",
+            "_schemaURL": "https://raw.githubusercontent.com/OpenLineage/OpenLineage/main/spec/OpenLineage.json#/definitions/BaseFacet",
+            "name": "test-lineage-namespace",
+            "jobState": "running",
+            "uniqueName": "TEST.test-dag.test-task",
+            "displayName": "test-dag.test-task",
+            "dagId": "test-dag",
+            "taskId": "test-task",
+            "cluster": "TEST",
+        },
+        "another_run_facet": {"name": "another-lineage-namespace"},
+    }
+
+
+@conf_vars(
+    {("openlineage", "custom_facet_functions"): "invalid_function"},
+)
+def test_get_custom_facets_with_invalid_function_definition():
+    result = get_custom_facets(SAMPLE_TI)
+    assert result == {}
+
+
+@conf_vars(
+    {
+        (
+            "openlineage",
+            "custom_facet_functions",
+        ): "tests.providers.openlineage.utils.custom_facet_fixture.return_type_is_not_dict"
+    },
+)
+def test_get_custom_facets_with_wrong_return_type_function():
+    result = get_custom_facets(SAMPLE_TI)
+    assert result == {}
